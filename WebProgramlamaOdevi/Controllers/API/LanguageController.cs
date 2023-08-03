d48420f22c5bd50e8edcf1b602453f2085ec84d0
nkemalarsu <nkemalarsu@gmail.com>
2023-08-03 10:54:32 +0300

    Çoklu dil desteği için gerekli controller'lar oluşturuldu

---
+++ WebProgramlamaOdevi/Controllers/API/LanguageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace WebProgramlamaOdevi.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class LanguageController : Controller
+    {
+        private readonly IStringLocalizer<LanguageController> _localizer;
+        public LanguageController(IStringLocalizer<LanguageController> localizer)
+        {
+            _localizer = localizer;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var guid = Guid.NewGuid();
+            return Ok(_localizer["RandomGUID", guid.ToString()].Value);
+        }
+    }
+}
